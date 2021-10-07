2ff7052a6fa8576405860ea6ee86b457ea35cb6d
Alice Russell <alice@heysparkbox.com>
2021-10-07 08:32:22 -0500

    test: add test to Header

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,9 +1,8 @@
-import * as React from "react";
-import { render, cleanup, screen } from '@testing-library/react';
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import TestRenderer from 'react-test-renderer';
 
-import { Header } from ".";
+import { Header } from '.';
 
 describe('the Header component', () => {
   test('renders a header element', () => {
